a1ae101d54b233794838e5ff10f534a09ecd7589
Mamungithube <orroshidmdmamun50@gmail.com>
2026-07-15 12:27:03 +0600

    responce time increase

--- scans/views.py
+++ scans/views.py
@@ -12,6 +12,8 @@ from .pagination import ScanListPagination
 from .filters import ScanDateFilter
 from dashboard.pagination import CustomDashboardPagination
 from .pdf_generator import generate_scan_pdf 
+import time
+
 
 class ScanViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
@@ -30,6 +32,19 @@ class ScanViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+    def retrieve(self, request, *args, **kwargs):
+        scan = self.get_object()
+        
+        max_wait = 30
+        waited = 0
+        
+        while scan.status == Scan.Status.PROCESSING and waited < max_wait:
+            time.sleep(1)
+            scan.refresh_from_db()
+            waited += 1
+        
+        serializer = self.get_serializer(scan)
+        return Response(serializer.data)
     @transaction.atomic # BEST PRACTICE FIX: Ensure Scan creation is atomic before task is deferred
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
